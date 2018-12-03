3ff6989a73ba36cbe28a57b1f685a9888e97651f
datadavev <datadavev@users.noreply.github.com>
2018-12-03 15:46:20 -0500

    Added nbsphinx dependency

--- source/conf.py
+++ source/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'plantweb.directive',
     #'sphinxcontrib.bibtex',
     'sphinx.ext.graphviz',
+    'nbsphinx',
     ]
 
 # Support for Markdown as a source type
@@ -84,7 +85,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["MN/**", ]
+exclude_patterns = ["_build", "**.ipynb_checkpoints", "MN/**", ]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
